34151d5bc02d020b0e00106a1e2658da6cff54b4
Brian Goff <cpuguy83@gmail.com>
2026-01-08 10:24:16 -0800

    tests: allow custom vscode path
    On nixos downloading/executing vscode (beyond the security concerns)
    does not work at all since the binary needs to be wrapped to include all
    the libs.
    
    This change lets me set a custom VSCODE_PATH to execute and run the
    tests.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from '@vscode/test-cli';
 
+const useCustomInstallation = Boolean(process.env.VSCODE_PATH);
+const skipExtensionDependencies =
+  process.env.VSCODE_SKIP_EXTENSION_DEPENDENCIES === '1' || useCustomInstallation;
+
 export default defineConfig({
   files: 'out/test/**/*.test.js',
   version: 'stable',
+  useInstallation: useCustomInstallation ? { fromPath: process.env.VSCODE_PATH } : undefined,
+  skipExtensionDependencies: skipExtensionDependencies || undefined,
   workspaceFolder: '.',
   mocha: {
     ui: 'tdd',
